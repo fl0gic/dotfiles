25d61f3edfb53e3f10f470ca3aeadb20a72b8421
flogic <gamerking195@gmail.com>
2020-12-17 14:22:08 -0700

    Tweaks to updates and zshrc

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ npm install npm -g
 npm update -g
 
 # Pip global packages
-python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 python3 -m pip install -U
+# python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 python3 -m pip install -U
 
 # Google Cloud & Flutter
 gcloud components update
