4ba598a8e6ac01ec9eff9279200958f9ae3ba031
Caden Kriese <13630061+cadenkriese@users.noreply.github.com>
2021-07-03 16:25:55 -0600

    Update brewfile

--- update.sh
+++ update.sh
@@ -5,11 +5,13 @@ sudo softwareupdate -i -a
 brew upgrade
 
 # Update npm & packages
-npm install npm -g
 npm update -g
 
-# Pip global packages
-# python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 python3 -m pip install -U
+#  Anaconda packages
+conda update --all
+
+# Gems
+sudo gem update
 
 # Google Cloud & Flutter
 gcloud components update
